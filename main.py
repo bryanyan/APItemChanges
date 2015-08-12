f54125a1208d7a12c469698ea750ae2a3cadd40c
Warren W Tian <warrent888@gmail.com>
2015-08-12 11:04:53 -0700

    requests work

--- main.py
+++ main.py
@@ -4,20 +4,6 @@ from pprint import pprint
 
 API_KEY = "b428aebe-05e5-4353-b569-980bcbeaa394"
 
-def main():
-    patches = ['5.11', '5.14']
-    winRates = {}
-    itemRates = {}
-    gameID = [getGameIds(patches[0]), getGameIds(patches[1])]
-    for patch in gameID:
-        for game in gameID[patch]:
-            gameData = getGameInfo(game)
-            #dosomething with gameData
-
-
-if __name__ == "__main__":
-    main()
-
 def getGameIds(patch):
     with open('AP_ITEM_DATASET/' + patch + '/RANKED_SOLO/NA.json') as data_file:    
         data = json.load(data_file)
@@ -29,5 +15,22 @@ def getGameInfo(id):
     response = api.getMatch("match", id)
     pprint(response)
     return response
+
+def main():
+    patches = ['5.11', '5.14']
+    winRates = {}
+    itemRates = {}
+    gameID = [getGameIds(patches[0]), getGameIds(patches[1])]
+    for patch in gameID:
+        for game in patch:
+            gameData = getGameInfo(game)
+            #dosomething with gameData
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 getGameInfo(getGameIds('5.11')[0])
