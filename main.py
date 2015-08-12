32d194403cbf89c4dc384c078001c12ac425d289
Warren W Tian <warrent888@gmail.com>
2015-08-12 10:54:15 -0700

    added some logic

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from api import RiotAPI
+import json
+from pprint import pprint
 
 def main():
-	api = RiotAPI("b428aebe-05e5-4353-b569-980bcbeaa394", "na")
-	response = api.getMatch("match", "1852548676")
-	print response
+    winRates = {}
+    itemRates = {}
+    gameID = [getGameIds('5.11'), getGameIds('5.14')]
+    for patch in gameID:
+        for game in gameID[patch]:
+            gameData = getGameInfo(game)
+            #dosomething with gameData
+
+
+
 
 if __name__ == "__main__":
-	main()
+    main()
 
+def getGameIds(patch):
+    with open('AP_ITEM_DATASET/' + patch + '/RANKED_SOLO/NA.json') as data_file:    
+        data = json.load(data_file)
+    # pprint(data)
+    return data
+
+def getGameInfo(id):
+    api = RiotAPI("b428aebe-05e5-4353-b569-980bcbeaa394", "na")
+    response = api.getMatch("match", id)
+    pprint(response)
+    return response
+
+getGameInfo(getGameIds('5.11')[0])
