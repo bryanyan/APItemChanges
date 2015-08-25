963d57be0a469dbd929320e9ac6eac50d1f18db1
Bryan Yan <bryanyan@andrew.cmu.edu>
2015-08-24 18:25:18 -0700

    updated script

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 from pprint import pprint
 import copy
 
+API_KEY = "637897ec-2820-4790-bf2c-984f1e0bf98f"
+
 def getGameIds(patch):
     with open('AP_ITEM_DATASET/' + patch + '/RANKED_SOLO/EUW.json') as data_file:    
         data = json.load(data_file)
@@ -50,8 +52,8 @@ def parse():
         champions = json.load(champFile)
 
     initialize = {}
-    for key in champions:
-        initialize[key] = {
+    for champ in champions:
+        initialize[champions[champ]['key']] = {
                         "pick": 0,
                         "wins": 0, 
                         "bans": 0, 
@@ -63,37 +65,67 @@ def parse():
                         "deaths": 0,
                         "assists": 0,
                         "neutralMinionsKilled": 0,
-                        "name": key 
+                        "name": champ
                         }
 
     for index in xrange(7):
         temp = copy.deepcopy(initialize)
         tiers[index] = temp
 
-    while (i < 10002):
-        try:
-            with open('FULL_GAMES_JSON/5.11/NA/gameNumber%d.json' %(i)) as data_file:
-                data = json.load(data_file)
-            requestString = ""
-            for participant in data['participantIdentities']:
-                requestString += str(participant['player']['summonerId'])
-                requestString += ","
-            requestString = requestString[:-1]
-            league = getLeagueInfo(requestString)
-            pprint (league)
-        except:
-            continue
+#########################################
+#fix efficiency
+#########################################
 
+    #while (i < 10002):
+    #try:
+    with open('FULL_GAMES_JSON/5.11/NA/gameNumber%d.json' %(i)) as data_file:
+        data = json.load(data_file)
+    requestString = ""
+    tDict = {}
+    for participant in data['participantIdentities']:
+        requestString += str(participant['player']['summonerId'])
+        tDict[str(participant['player']['summonerId'])] = [participant['participantId'], None] 
+        requestString += ","
+    requestString = requestString[:-1]
+    league = getLeagueInfo(requestString)
+    for key in league.keys():
+        medal = league[key][0]['tier']
+        for identity in tDict:
+            if key == identity:
+                tDict[identity][1] = medal
+    
+    for player in data['participants']:
+        for playerID in tDict:
+            if player['participantId'] == tDict[playerID][0]:
+                if tDict[playerID][1] == 'BRONZE':
+                    tiers[0][str(player['championId'])]['pick'] += 1
+                if tDict[playerID][1] == 'SILVER': 
+                    tiers[1]
+                if tDict[playerID][1] == 'GOLD': 
+                    tiers[2]
+                if tDict[playerID][1] == 'PLATINUM': 
+                    tiers[3]
+                if tDict[playerID][1] == 'DIAMOND': 
+                    tiers[4]
+                if tDict[playerID][1] == 'MASTER': 
+                    tiers[5]
+                if tDict[playerID][1] == 'CHALLENGER': 
+                    tiers[6]
+
+
+    #except:
+        #continue
 
     #with io.open('NAdata.json', 'w', encoding='utf-8') as fout:
-    #    fout.write(unicode(json.dumps(NAdata, ensure_ascii=False)))
+        #fout.write(unicode(json.dumps(NAdata, ensure_ascii=False)))
 
-
-
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+    #main()
 
 #def hardcode():
     #data = getGameInfo(getGameIds('5.11')[2])
     #with io.open('gameNumber3.json', 'w', encoding='utf-8') as fout:
         #fout.write(unicode(json.dumps(data, ensure_ascii=False)))
+
+
+parse()
